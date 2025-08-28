aca2667c86f806ea8ba26cc09990c65579df5ed3
Steven Breuer <stevenbreuer@icloud.com>
2025-08-28 20:55:49 +0200

    Aktualisiere die App-Version auf 1.0.4 in der Versionsprüfung, um die neuesten Änderungen zu reflektieren.

--- app.js
+++ app.js
@@ -205,7 +205,7 @@
   });
 
   // Version checking
-  const currentVersion = '1.0.3';
+  const currentVersion = '1.0.4';
   const githubRepo = 'Stevbreu/PWA_Sepsis'; // Ersetze mit deinem GitHub Repository
   
   async function checkForUpdates(isAutomatic = false) {
